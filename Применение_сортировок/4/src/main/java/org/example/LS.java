7601fd8f248a514f8e39a59c5ac031602d101b3f
Esepenok Alexandr <145503317+Chawotto@users.noreply.github.com>
2024-11-25 10:38:43 +0300

    Update LS.java

--- Применение_сортировок/4/src/main/java/org/example/LS.java
+++ Применение_сортировок/4/src/main/java/org/example/LS.java
@@ -31,50 +31,44 @@ public class LS {
         }
 
         List<File> fileList = new ArrayList<>(Arrays.asList(files));
-        showMenu(new Scanner(System.in), fileList);
+        List<Comparator<File>> comparators = new ArrayList<>();
 
-        if (logger.isLoggable(Level.INFO)) {
-            logger.info("Список файлов:");
-            for (File file : fileList) {
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "-t":
+                    comparators.add(Comparator.comparingLong(File::lastModified));
+                    break;
+                case "-T":
+                    comparators.add(Comparator.comparingLong(File::lastModified).reversed());
+                    break;
+                case "-n":
+                    comparators.add(Comparator.comparing(File::getName));
+                    break;
+                case "-N":
+                    comparators.add(Comparator.comparing(File::getName).reversed());
+                    break;
+                case "-s":
+                    comparators.add(Comparator.comparingLong(File::length));
+                    break;
+                case "-S":
+                    comparators.add(Comparator.comparingLong(File::length).reversed());
+                    break;
+                default:
+                    if (logger.isLoggable(Level.INFO)) {
+                        logger.severe(String.format("Неверный аргумент сортировки: %s", args[i]));
+                    }
+                    return;
+            }
+        }
+
+        comparators.forEach(fileList::sort);
+
+        logger.info("Список файлов:");
+        for (File file : fileList) {
+            if (logger.isLoggable(Level.INFO)) {
                 logger.info(String.format("Имя: %s, Размер: %d байт, Последнее изменение: %tc",
                         file.getName(), file.length(), file.lastModified()));
             }
-        }
-    }
-
-    private static void showMenu(Scanner scanner, List<File> files) {
-        logger.info("Выберите параметр сортировки:");
-        logger.info("1. По имени (возрастание)");
-        logger.info("2. По имени (убывание)");
-        logger.info("3. По размеру (возрастание)");
-        logger.info("4. По размеру (убывание)");
-        logger.info("5. По дате изменения (возрастание)");
-        logger.info("6. По дате изменения (убывание)");
-        logger.info("Ваш выбор (1-6): ");
-
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                files.sort(Comparator.comparing(File::getName));
-                break;
-            case 2:
-                files.sort(Comparator.comparing(File::getName).reversed());
-                break;
-            case 3:
-                files.sort(Comparator.comparingLong(File::length));
-                break;
-            case 4:
-                files.sort(Comparator.comparingLong(File::length).reversed());
-                break;
-            case 5:
-                files.sort(Comparator.comparingLong(File::lastModified));
-                break;
-            case 6:
-                files.sort(Comparator.comparingLong(File::lastModified).reversed());
-                break;
-            default:
-                logger.severe("Неверный выбор. Сортировка не выполнена.");
-                return;
         }
     }
 }
